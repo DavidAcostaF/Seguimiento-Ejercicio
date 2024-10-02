16a19e8e9c2f4749ada3d142033388e2dc998cb1
David Acosta Fajardo <108051764+DavidAcostaF@users.noreply.github.com>
2024-10-01 19:15:26 -0700

    primer commit

---
+++ SeguimientoEjercicioNegocio/src/main/java/com/mycompany/seguimientoejercicionegocio/SeguimientoEjercicioNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.seguimientoejercicionegocio;
+
+/**
+ *
+ * @author af_da
+ */
+public class SeguimientoEjercicioNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ SeguimientoEjercicioPersistencia/src/main/java/com/mycompany/seguimientoejerciciopersistencia/SeguimientoEjercicioPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.seguimientoejerciciopersistencia;
+
+/**
+ *
+ * @author af_da
+ */
+public class SeguimientoEjercicioPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
