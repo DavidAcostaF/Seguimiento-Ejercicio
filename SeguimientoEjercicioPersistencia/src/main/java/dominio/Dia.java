b87c80e75c32a505e780dce9031b779b56504a2e
rsarayos <raul.sauceda@potros.itson.edu.mx>
2024-10-02 21:39:30 -0700

    ajuste

--- SeguimientoEjercicioPersistencia/src/main/java/dominio/Dia.java
+++ SeguimientoEjercicioPersistencia/src/main/java/dominio/Dia.java
@@ -19,7 +19,7 @@ import javax.persistence.Table;
 public class Dia implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @Enumerated(EnumType.STRING)
